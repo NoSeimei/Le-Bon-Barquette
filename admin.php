774f5eaa077e7c2f18934270e91e70cefe79ea3b
NoSeimei <52064555+NoSeimei@users.noreply.github.com>
2020-11-21 14:19:28 +0400

    modif

--- admin.php
+++ admin.php
@@ -6,29 +6,29 @@ include("Class/Plats.php");
 include("Class/Dessert.php");
 include("Class/Admin.php");
 include("Function/Function.php");
-try{
+try {
     $request = $db->query("SELECT * FROM entree");
-$request->setFetchMode(PDO::FETCH_CLASS,'Entree');
-$lesEntree = $request->fetchAll();
-}catch(Exception $exE){
+    $request->setFetchMode(PDO::FETCH_CLASS, 'Entree');
+    $lesEntree = $request->fetchAll();
+} catch (Exception $exE) {
 
     echo $exE;
 }
 
-try{
-$request2 = $db->query("SELECT * FROM plats");
-$request2->setFetchMode(PDO::FETCH_CLASS,'Plats');
-$lesPlats = $request2->fetchAll();
-}catch(Exception $exP){
+try {
+    $request2 = $db->query("SELECT * FROM plats");
+    $request2->setFetchMode(PDO::FETCH_CLASS, 'Plats');
+    $lesPlats = $request2->fetchAll();
+} catch (Exception $exP) {
 
     echo $exP;
 }
 
-try{
-$request3 = $db->query("SELECT * FROM dessert");
-$request3->setFetchMode(PDO::FETCH_CLASS,'Dessert');
-$lesDesserts = $request3->fetchAll();
-}catch(Exception $exD){
+try {
+    $request3 = $db->query("SELECT * FROM dessert");
+    $request3->setFetchMode(PDO::FETCH_CLASS, 'Dessert');
+    $lesDesserts = $request3->fetchAll();
+} catch (Exception $exD) {
 
     echo $exD;
 }
@@ -52,8 +52,8 @@ $lesDesserts = $request3->fetchAll();
 
 <body>
 
-<div class="container-admin">
-<h1 style="color:black; text-align:center" >Dashboard Admin</h1><br>
+    <div class="container-admin">
+        <h1 style="color:black; text-align:center">Dashboard Admin</h1><br>
         <div class="subContainer">
             <div class="left">
                 <table class="table">
@@ -67,18 +67,17 @@ $lesDesserts = $request3->fetchAll();
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach($lesEntree as $entree)
-        {   ?>
-                        <tr>
-                            <th scope="row"><?php echo $entree->getImage();?></th>
-                            <td><?php echo $entree->getNom();?></td>
-                            <td><?php echo $entree->getDescription();?></td>
-                            <td><?php echo $entree->getPrix_Entree();?></td>
-                            <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
-                        </tr>
+                        <?php foreach ($lesEntree as $entree) {   ?>
+                            <tr>
+                                <th scope="row"><?php echo $entree->getImage(); ?></th>
+                                <td><?php echo $entree->getNom(); ?></td>
+                                <td><?php echo $entree->getDescription(); ?></td>
+                                <td><?php echo $entree->getPrix_Entree(); ?></td>
+                                <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
+                            </tr>
                         <?php
-        }
-      ?>
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -95,18 +94,17 @@ $lesDesserts = $request3->fetchAll();
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach($lesPlats as $plat)
-        {   ?>
-                        <tr>
-                            <th scope="row"><?php echo $plat->getImage();?></th>
-                            <td><?php echo $plat->getNom();?></td>
-                            <td><?php echo $plat->getDescription();?></td>
-                            <td><?php echo $plat->getPrix_Plat();?></td>
-                            <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
-                        </tr>
+                        <?php foreach ($lesPlats as $plat) {   ?>
+                            <tr>
+                                <th scope="row"><?php echo $plat->getImage(); ?></th>
+                                <td><?php echo $plat->getNom(); ?></td>
+                                <td><?php echo $plat->getDescription(); ?></td>
+                                <td><?php echo $plat->getPrix_Plat(); ?></td>
+                                <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
+                            </tr>
                         <?php
-        }
-      ?>
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -123,18 +121,17 @@ $lesDesserts = $request3->fetchAll();
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach($lesDesserts as $dessert)
-        {   ?>
-                        <tr>
-                            <th scope="row"><?php echo $dessert->getImage();?></th>
-                            <td><?php echo $dessert->getNom();?></td>
-                            <td><?php echo $dessert->getDescription();?></td>
-                            <td><?php echo $dessert->getPrix_Dessert();?></td>
-                            <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
-                        </tr>
+                        <?php foreach ($lesDesserts as $dessert) {   ?>
+                            <tr>
+                                <th scope="row"><?php echo $dessert->getImage(); ?></th>
+                                <td><?php echo $dessert->getNom(); ?></td>
+                                <td><?php echo $dessert->getDescription(); ?></td>
+                                <td><?php echo $dessert->getPrix_Dessert(); ?></td>
+                                <td><a href="" class="far fa-edit"></a> <a href="" style="color:red" class="far fa-trash-alt"></a></td>
+                            </tr>
                         <?php
-        }
-      ?>
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -150,14 +147,11 @@ $lesDesserts = $request3->fetchAll();
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 </body>
 

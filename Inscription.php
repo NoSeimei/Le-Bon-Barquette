99e6d0bfbcaaf50b3769a9082099f5d37d2a0db8
Toianf <72868197+Toianf@users.noreply.github.com>
2020-11-23 14:25:54 +0400

    Update Inscription.php

--- Inscription.php
+++ Inscription.php
@@ -237,7 +237,7 @@
 						<div class="wrap-btn-booking flex-c-m m-t-6">
 							<!-- Button3 -->
 							<button type="submit" class="btn3 flex-c-m size13 txt11 trans-0-4">
-								S'inscrire
+								Se connecter
 							</button>
 						</div>
 					</form>
